47e5411d14cc3ff493b9f09de2ebc0031a56b5ed
diegodelias <diegodelias@gmail.com>
2022-12-05 18:24:00 -0300

    testimonios segundo commit

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -1,102 +1,117 @@
 import React, { useState } from "react";
 import "./estilos.css";
 import img1 from "./imagenes/img1.jpg";
+import img2 from "./imagenes/img2.jpg";
+import img3 from "./imagenes/img3.jpg";
+
+
+import fondo from "./imagenes/fondo.jpg";
 const Testimonial = () => {
   return (
-    <div className="contenedor">
-      <div className="container">
-        <div className=" titulo  my-5">
-          <h1 className="text-center text-white text-uppercase">Tesimonios</h1>
-        </div>
+<div >
 
-        <div
-          id="carouselExampleCaptions"
-          className="carousel slide"
-          data-bs-ride="carousel"
-        >
-          <div className="carousel-indicators">
-            <button
-              type="button"
-              data-bs-target="#carouselExampleCaptions"
-              data-bs-slide-to="0"
-              class="active"
-              aria-current="true"
-              aria-label="Slide 1"
-            ></button>
-            <button
-              type="button"
-              data-bs-target="#carouselExampleCaptions"
-              data-bs-slide-to="1"
-              aria-label="Slide 2"
-            ></button>
-            <button
-              type="button"
-              data-bs-target="#carouselExampleCaptions"
-              data-bs-slide-to="2"
-              aria-label="Slide 3"
-            ></button>
+
+
+
+  
+
+<div class="testimonial-area" style={{backgroundImage: `url(${fondo})` , backgroundPosition: 'center',
+  backgroundSize: '100% auto' , backgroundRepeat: 'no-repeat'
+  }}>
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12">
+            <div class="section-header text-center">
+              <h4>Testimonios</h4>
+              <h2>Lo que opinan nuestros clientes</h2>
+              {/* <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Quaerat at animi unde, et, obcaecati eius.
+              </p> */}
+            </div>
           </div>
-          <div className="carousel-inner">
-        
-            <div className="carousel-item active">
-            <div className="row">
-                <div className="col-md-4 mb-3">
-                  <div className="card text-center">
-                    <img className="card-img-top" src={img1} />
-
-                    <div className="card-body">
-                      <h4>
-                        Rone Galle <br />
-                        <span>Project Manager</span>
-                      </h4>
-                      <p className="card-text">
-                        Lorem ipsum, dolor sit amet consectetur adipisicing
-                        elit. Officia illum perspiciatis laboriosam obcaecati
-                        molestiae architecto.
-                      </p>
+          <div class="col-sm-12">
+            <div
+              id="carouselExampleIndicators"
+              class="carousel slide"
+              data-bs-ride="true"
+            >
+              <div class="carousel-indicators">
+                <button
+                  type="button"
+                  data-bs-target="#carouselExampleIndicators"
+                  data-bs-slide-to="0"
+                  class="active"
+                  aria-current="true"
+                  aria-label="Slide 1"
+                ></button>
+                <button
+                  type="button"
+                  data-bs-target="#carouselExampleIndicators"
+                  data-bs-slide-to="1"
+                  aria-label="Slide 2"
+                ></button>
+                <button
+                  type="button"
+                  data-bs-target="#carouselExampleIndicators"
+                  data-bs-slide-to="2"
+                  aria-label="Slide 3"
+                ></button>
+              </div>
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <div class="icon-area">
+                    <i class="fa fa-comments"></i>
+                  </div>
+                  <div class="content text-center">
+                    <p>
+                     "Honestos, muy honestos…, cercanos, súper-profesionales!, lo cierto es que me resultan «rara avis» en un mundo (el veterinario) demasiado masificado y cada vez más carente de deontología…, a los que no les conozcan les invito, les reto, a que lo hagan; para nosotros fue una grandísima suerte cruzarnos con ellos. Multitud de servicios con el espacio y los medios imprescindibles, sin excesos y sin arrogancia…, fidelizan al cliente."
+                    </p>
+                    <div class="person">
+                      <img src={img1} alt="" />
                     </div>
+                    <h5>Néstor</h5>
+                    
                   </div>
                 </div>
-                <div class="col-md-4 mb-3">
-                  <div className="card text-center">
-                    <img className="card-img-top" src={img1} />
-
-                    <div className="card-body">
-                      <h4>
-                        Rone Galle <br />
-                        <span>Project Manager</span>
-                      </h4>
-                      <p className="card-text">
-                        Lorem ipsum, dolor sit amet consectetur adipisicing
-                        elit. Officia illum perspiciatis laboriosam obcaecati
-                        molestiae architecto.
-                      </p>
+                <div class="carousel-item">
+                  <div class="icon-area">
+                    <i class="fa fa-comments"></i>
+                  </div>
+                  <div class="content text-center">
+                    <p>
+                    Desde hace 9 años  que se encargan de mis perros. Quizá lo que más me gusta de ellos es la cercanía que tienen, el salir siempre con la sensación de que mis perros estánen buenas manos. Me gusta como trabajan en equipo y como se unen para diagnosticar y pautar el tratamiento. He tenido buenísima experiencia con ello en alguna intervención y me ha gustado que son veterinarios muy relacionados con otros profesionales del gremio, lo cual hace que su servicio al cliente sea de una profesionalidad máxima
+                    </p>
+                    <div class="person">
+                      <img src={img2} alt="" />
                     </div>
+                    <h5>Martin </h5>
+                    
                   </div>
                 </div>
-                <div class="col-md-4 mb-3">
-                  <div className="card text-center">
-                    <img className="card-img-top" src={img1} />
-
-                    <div className="card-body">
-                      <h4>
-                        Rone Galle <br />
-                        <span>Project Manager</span>
-                      </h4>
-                      <p className="card-text">
-                        Lorem ipsum, dolor sit amet consectetur adipisicing
-                        elit. Officia illum perspiciatis laboriosam obcaecati
-                        molestiae architecto.
-                      </p>
+                <div class="carousel-item">
+                  <div class="icon-area">
+                    <i class="fa fa-comments"></i>
+                  </div>
+                  <div class="content text-center">
+                    <p>
+                    Parte de mi familia que siempre han cuidado de mis seres más queridos con el mismo interés y cariño que le pondrían a cualquiera de sus propios perros. Sin duda alguna profesionales en los que confiar con los ojos cerrados. Un 10 por la clínica Club de Campo!!!
+                    </p>
+                    <div class="person">
+                      <img src={img3} alt="" />
                     </div>
+                    <h5>Mauro</h5>
+                  
                   </div>
                 </div>
-                
               </div>
             </div>
           </div>
         </div>
       </div>
+    </div>
+      
+       
     </div>
   );
 };
