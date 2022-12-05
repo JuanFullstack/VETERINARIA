ddaf863c7799c6faccc2c1379f07253ed80cad47
diegodelias <diegodelias@gmail.com>
2022-12-05 18:31:19 -0300

    commit 3 testimonial

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -67,7 +67,7 @@ const Testimonial = () => {
                     <p>
                      "Honestos, muy honestos…, cercanos, súper-profesionales!, lo cierto es que me resultan «rara avis» en un mundo (el veterinario) demasiado masificado y cada vez más carente de deontología…, a los que no les conozcan les invito, les reto, a que lo hagan; para nosotros fue una grandísima suerte cruzarnos con ellos. Multitud de servicios con el espacio y los medios imprescindibles, sin excesos y sin arrogancia…, fidelizan al cliente."
                     </p>
-                    <div class="person">
+                    <div class="persona">
                       <img src={img1} alt="" />
                     </div>
                     <h5>Néstor</h5>
@@ -82,7 +82,7 @@ const Testimonial = () => {
                     <p>
                     Desde hace 9 años  que se encargan de mis perros. Quizá lo que más me gusta de ellos es la cercanía que tienen, el salir siempre con la sensación de que mis perros estánen buenas manos. Me gusta como trabajan en equipo y como se unen para diagnosticar y pautar el tratamiento. He tenido buenísima experiencia con ello en alguna intervención y me ha gustado que son veterinarios muy relacionados con otros profesionales del gremio, lo cual hace que su servicio al cliente sea de una profesionalidad máxima
                     </p>
-                    <div class="person">
+                    <div class="persona">
                       <img src={img2} alt="" />
                     </div>
                     <h5>Martin </h5>
@@ -97,7 +97,7 @@ const Testimonial = () => {
                     <p>
                     Parte de mi familia que siempre han cuidado de mis seres más queridos con el mismo interés y cariño que le pondrían a cualquiera de sus propios perros. Sin duda alguna profesionales en los que confiar con los ojos cerrados. Un 10 por la clínica Club de Campo!!!
                     </p>
-                    <div class="person">
+                    <div class="persona">
                       <img src={img3} alt="" />
                     </div>
                     <h5>Mauro</h5>
