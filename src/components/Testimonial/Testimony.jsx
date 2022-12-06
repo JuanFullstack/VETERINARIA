35bdde7ea0547283b770a2d8656a846d86c68151
dcazabat <dacazabat@gmail.com>
2022-12-06 16:19:02 -0300

    A revisar por QA

--- src/components/Testimonial/Testimony.jsx
+++ src/components/Testimonial/Testimony.jsx
@@ -1,18 +1,38 @@
-export default function Testimony(props) {
-    return (
-        <div class="carousel-item">
-            <div class="icon-area">
-                <i class="fa fa-comments"></i>
-            </div>
-            <div class="content text-center">
-                <p>
-                    {props.testimony}
-                </p>
-                <div class="persona">
-                    <img src={props.img} alt="" />
+export default function Testimony({props}, {first}) {
+    if (props.id === 1) {
+        return (
+            <div className="carousel-item active">
+                <div className="icon-area">
+                    <i className="fa fa-comments"></i>
                 </div>
-                <h5>{props.name} </h5>
+                <div className="content text-center">
+                    <p>
+                        {props.testimony}
+                    </p>
+                    <div className="persona">
+                        <img src={props.img} alt="" />
+                    </div>
+                    <h5>{props.name} </h5>
+                </div>
             </div>
-        </div>
-    )
+        )
+    } else {
+        return (
+            <div className="carousel-item">
+                <div className="icon-area">
+                    <i className="fa fa-comments"></i>
+                </div>
+                <div className="content text-center">
+                    <p>
+                        {props.testimony}
+                    </p>
+                    <div className="persona">
+                        <img src={props.img} alt="" />
+                    </div>
+                    <h5>{props.name} </h5>
+                </div>
+            </div>
+        )
+    }
+
 }
