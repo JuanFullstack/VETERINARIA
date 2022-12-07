eec1486db31a692777e68b021dd54df494ed4e8a
diegodelias <diegodelias@gmail.com>
2022-12-07 15:10:14 -0300

    inicio nosotros

--- src/components/Nosotros/NosotrosComponente.jsx
+++ src/components/Nosotros/NosotrosComponente.jsx
@@ -1,52 +1,236 @@
 import React, { useState } from "react";
 import "./estilos.css";
-import casaperro from "./imagenes/casaperro.jpg"
 
 const NosotrosComponente = () => {
+  const [toggleTab, setToggleTab] = useState(1);
+  const toogleState = (index) => {
+    setToggleTab(index);
+  };
   return (
-<div >
-<div class="container"> 
-        <div class="col-xs-12 col-lg-5 iso" >
-            <img src={casaperro} alt=""/>
-     </div>
+    <div>
+      <section className="nosotros">
+        <div className="container">
+        <div className="row">
+          <div className="column">
+            <div className="nosotros-img"></div>
+          </div>
 
-     <div class="col-xs-12 ">
+          <div className="column">
+            <div className="tabs">
+              <div
+                className={
+                  toggleTab === 1 ? "single-tab active-tab" : "single-tab"
+                }
+                onClick={() => toogleState(1)}
+              >
+                <h2>Nosotros</h2>
+              </div>
+              <div
+                className={
+                  toggleTab === 2 ? "single-tab active-tab" : "single-tab"
+                }
+                onClick={() => toogleState(2)}
+              >
+                <h2>Servicios</h2>
+              </div>
+
+              <div
+                className={
+                  toggleTab === 3 ? "single-tab active-tab" : "single-tab"
+                }
+                onClick={() => toogleState(3)}
+              >
+                <h2>Especialidades</h2>
+              </div>
+            </div>
+
+            <div className="tab-content">
+              {/* contenido about */}
+              <div
+                className={
+                  toggleTab === 1
+                    ? "contenidoNosotros active-content"
+                    : "contenidoNosotros"
+                }
+              >
+                <h2>Historia</h2>
+                <p>
+                   Contamos con excelentes profesionales en el área de clínica médica y quirúrgica.
+                  También ofrecemos el servicio en varias especialidades de Medicina Veterinaria,
+                   con reconocidos médicos veterinarios.Además, se realizan todo tipo de intervenciones 
+                   quirúrgicas desde general hasta cirugía especializada.
+                </p>
+                <h3>Misión</h3>
+                <p>
+                Buscamos el bienestar integral de las mascotas ofreciendo servicios de calidad, fortaleciendo el 
+                vínculo entre personas y animales a través de la toma de conciencia
+                 y responsabilidad de los propietarios. La innovación y actualización son parte de nuestras herramientas.ibusdam.
+                </p>
+              </div>
+
+              {/* Skill Content */}
+              <div className="tab-content">
+                {/* contenido about */}
+                <div className={
+                  toggleTab === 2
+                    ? "contenidoNosotros active-content"
+                    : "contenidoNosotros"
+                }>
+                  <h2>Servicios</h2>
+                  <p>
+                  Contamos con los mejores Médicos Veterinarios con formación académica de calidad para ofrecerles las 
+                  herramientas necesarias para cuidar de tu mascota. Consultá todas nuestras sucursales de hospitales 
+                  veterinarios y visítanos para poder ayudarte y ayudar a tu compañero
+                  </p>
+                  <div className="servicios-row">
+                    <div className="servicios-column">
+                      <div className="progress-wrap">
+                        <h3>INTERNAMIENTO</h3>
+                        <div className="progress">
+                          <div className="progress-bar">
+                            <span>80%</span>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+
+                   
+
+                    <div className="servicios-column">
+                      <div className="progress-wrap">
+                        <h3>MOVILIDAD</h3>
+                        <div className="progress">
+                          <div className="progress-bar">
+                            <span>90%</span>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="servicios-column">
+                      <div className="progress-wrap">
+                        <h3>ECOGRAFÍA Y RAYOS X</h3>
+                        <div className="progress">
+                          <div className="progress-bar">
+                            <span>90%</span>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    
+                    <div className="servicios-column">
+                      <div className="progress-wrap">
+                        <h3>URGENCIAS</h3>
+                        <div className="progress">
+                          <div className="progress-bar">
+                            <span>90%</span>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="servicios-column">
+                      <div className="progress-wrap">
+                        <h3>CLINICA GENERAL</h3>
+                        <div className="progress">
+                          <div className="progress-bar">
+                            <span>90%</span>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="servicios-column">
+                      <div className="progress-wrap">
+                        <h3>INTERCONSULTAS</h3>
+                        <div className="progress">
+                          <div className="progress-bar">
+                            <span>90%</span>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
 
 
-        <p class="texto"> Brindamos un servicio veterinario a domicilio en CABA para que las mascotas puedan recibir en su casa la atención que necesitan, sin las molestias que ocasionan el desplazarse a un centro veterinario.
-             La prevención es muy importante, por ende, el diagnóstico temprano de una enfermedad permite que haya mejores resultados en su tratamiento. Lógicamente, las intervenciones quirúrgicas, hospitalizaciones y radiografías no se pueden realizar a domicilio.
-              Para ello, trabajamos en conjunto con Centros de Internación para atender a casos más complejos.
-               Veterinaria en Casa brinda un servicio veterinario personalizado enfocado en fortalecer el vínculo entre la mascota y vos. 
-               Cada animal es un mundo, por eso contamos con especialistas de todo tipo para que puedas darle todas las atenciones a tu mascotas en el mismo lugar. Algunas de nuestras especialidades:
+
+
+                </div>
+                </div>
+
+                {/*Experience content */}
+
+                <div
+                  className={
+                    toggleTab === 3
+                      ? "contenidoNosotros active-content"
+                      : "contenidoNosotros"
+                  }
+                >
+                  <div className="exp-column">
+                    <h5>Anestesiología</h5>
+                    <span>Dr. Marco Velasquez</span>
+                  
+                  </div>
+                
+
                
-               Gastroenterología​
+                  <div className="exp-column mt-2">
+                    <h5>Ecografía</h5>
+                    <span>Dr. Jose Ramirez</span>
+                  
+                  </div>
+                
+
+                  <div className="exp-column mt-2">
+                    <h5>Odontología</h5>
+                    <span>Dr. Luis Roca</span>
+                 
+                  </div>
+
+                  
+                
+
                
-               Odontología 
+                  <div className="exp-column mt-2">
+                    <h5>Cirugía General</h5>
+                    <span>Dr. Luis Roca</span>
+                 
+                  </div>
 
-               Traumatología
-               Nefrología
-               Oftalmología
-               Cardiología
-               Acupuntura
-                Neurología
-               Fisioterapia
-               Hematología
-               Endocrinología
-               Oncología
-               Dermatología
+                  
+                
+
+                
+                  <div className="exp-column mt-2">
+                    <h5>Patología</h5>
+                    <span>Dr. Cesar Palacios</span>
+                 
+                  </div>
+
+                  
             
-            
-            
-            </p>
 
-     </div>
+               
+                  <div className="exp-column mt-2">
+                    <h5>Traumatología y ortopedia</h5>
+                    <span>Dr. Ricardo Rojas - Dr. Giuseppe Granados</span>
+                 
+                  </div>
+
+                  
+                
+
+                  </div>
+              
 
 
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
+      </section>
     </div>
-    
-
-
-</div>
   );
 };
 
